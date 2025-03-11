eb23b007df8a51ffb12d005eab5c8ea041c2d195
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2025-03-11 11:34:10 -0400

    replace class with functions

--- SideBySideComparisons.py
+++ SideBySideComparisons.py
@@ -1,47 +1,46 @@
-class SideBySideComparisons:
+def get_staircase1(input):
+    output = []
+    index = 0
+    steps = 1
+    while index + steps <= len(input) :
+        stairs = input[index : index + steps]
+        output.append(stairs)
+        steps += 1
+        index += steps - 1
+    if index < len(input) :
+        result = "False"
+    else :
+        result  = output
+    return result
 
-    def get_stairs(self, input):
-        output = []
-        index = 0
-        steps = 1
-        while index + steps <= len(input) :
-            stairs = input[index : index + steps]
-            output.append(stairs)
-            steps += 1
-            index += steps - 1
-        if index < len(input) :
-            result = "False"
-        else :
-            result  = output
-        return result
 
-    def create_staircase1(self, nums):
-      while len(nums) != 0:
-        step = 1
-        subsets = []
+def get_staircase2(nums):
+  while len(nums) != 0:
+    step = 1
+    subsets = []
+    if len(nums) >= step:
+      subsets.append(nums[0:step])
+      nums = nums[step:]
+      step += 1
+    else:
+      return False
+  return subsets
+
+
+def get_staircase3(nums):
+    step = 1
+    subsets = []
+    while len(nums) != 0:
         if len(nums) >= step:
-          subsets.append(nums[0:step])
-          nums = nums[step:]
-          step += 1
+            subsets.append(nums[0:step])
+            nums = nums[step:]
+            step += 1
         else:
-          return False
-      return subsets
+            return False
+    return subsets
 
-    def create_staircase2(self, nums):
-        step = 1
-        subsets = []
-        while len(nums) != 0:
-            if len(nums) >= step:
-                subsets.append(nums[0:step])
-                nums = nums[step:]
-                step += 1
-            else:
-                return False
-        return subsets
 
 if __name__ == '__main__':
-
-    my_stares = SideBySideComparisons()
 
     input11 = [1, 2, 3, 4, 5, 6]
     input22 = [1, 2, 3, 4, 5, 6, 7]
@@ -53,20 +52,20 @@ if __name__ == '__main__':
     print()
 
     print("my_stares")
-    print("Steps " + str(my_stares.get_stairs(input11)))
-    print("Steps " + str(my_stares.get_stairs(input22)))
-    print("Steps " + str(my_stares.get_stairs(input33)))
+    print("Steps " + str(get_staircase1(input11)))
+    print("Steps " + str(get_staircase1(input22)))
+    print("Steps " + str(get_staircase1(input33)))
     print()
 
-    print("create_staircase1")
-    print("Steps " + str(my_stares.create_staircase1(input11)))
-    print("Steps " + str(my_stares.create_staircase1(input22)))
-    print("Steps " + str(my_stares.create_staircase1(input33)))
+    print("get_staircase2")
+    print("Steps " + str(get_staircase2(input11)))
+    print("Steps " + str(get_staircase2(input22)))
+    print("Steps " + str(get_staircase2(input33)))
     print()
 
-    print("create_staircase2")
-    print("Steps " + str(my_stares.create_staircase2(input11)))
-    print("Steps " + str(my_stares.create_staircase2(input22)))
-    print("Steps " + str(my_stares.create_staircase2(input33)))
+    print("get_staircase3")
+    print("Steps " + str(get_staircase3(input11)))
+    print("Steps " + str(get_staircase3(input22)))
+    print("Steps " + str(get_staircase3(input33)))
 
 
