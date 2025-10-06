9a03cdbb33d4771aafeec5edac77e397a0a1423e
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2025-10-05 21:19:56 -0400

    pandas basic

--- pyex/w3pandas.py
+++ pyex/w3pandas.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+pd.options.display.max_rows = 200
 df = pd.read_csv('data.csv')
+print(pd.options.display.max_rows)
+print(df.info())
+print(df.loc[[0, 1]])
+print(df, end='\n\n')
 
-print(df)
+df = pd.read_json('data.json')
+print(df.to_string())
+
