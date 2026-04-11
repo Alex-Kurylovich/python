2841fb04dfb0de596c4a7b01e632db43b872ef20
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2026-04-11 17:53:46 -0400

    add bank renege example add comment

--- simpyex/examples/BankRenege.py
+++ simpyex/examples/BankRenege.py
@@ -8,6 +8,20 @@
 # If she gets to the counter, she uses it for a while before releasing it.
 # New customers are created by the source process every few time steps.
 
+"""
+Bank renege example
+
+Covers:
+
+- Resources: Resource
+- Condition events
+
+Scenario:
+  A counter with a random service time and customers who renege. Based on the
+  program bank08.py from TheBank tutorial of SimPy 2. (KGM)
+
+"""
+
 import random
 
 import simpy
